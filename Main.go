3cbe822648cdde2e55c7f5758cb9bdbe6621151f
JUJU-Arch <juju.jetstream@gmail.com>
2018-02-11 22:18:14 +0900

    オプションに対応

---
+++ Main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var index = arrayContains(os.Args, "-s")
+	if(index < 0){
+		fmt.Println("option not found")
+	}else{
+		fmt.Println("option found at " + strconv.Itoa(index))
+	}
+}
+
+func arrayContains(arr []string, s string) int{
+	for i, v := range arr{
+		if v == s{
+			return i
+		}
+	}
+	return -1
+}
