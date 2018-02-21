1c8e5717147dced508a199506a44d6a74b0d274d
JUJU-Arch <juju.jetstream@gmail.com>
2018-02-21 23:08:41 +0900

    オプションがどこに来ても動作するように変更

--- Main.go
+++ Main.go
@@ -6,19 +6,38 @@ import (
 )
 
 func main() {
+	// オプション関係の設定を読まないようにするための配列
+	isNotNeedRead := make([]bool, len(os.Args), len(os.Args))
+	isNotNeedRead[0] = true
+
+	// 引数がない場合にはエラーを返す
 	if len(os.Args) <= 1 {
 		fmt.Println("not enough argment")
 		return
 	}
+
+	// オプションの処理
+	sourceLang := ""
 	var index = arrayContains(os.Args, "-f")
-	if(index < 0){
-		fmt.Println(translateString(os.Args[1], "ja"))
+	// 元言語についての処理
+	if(index < 0) {
+		 sourceLang = "ja"
 	}else{
+		// オプションあるのに引数少ない
 		if len(os.Args) <= 3 {
 			fmt.Println("not enough argment")
 			return
-		}else {
-			fmt.Println(translateString(os.Args[1], os.Args[index + 1]))
+		}else{
+			sourceLang = os.Args[index + 1]
+			isNotNeedRead[index] = true
+			isNotNeedRead[index + 1] = true
+		}
+	}
+
+	// 翻訳の実行
+	for i, v := range(isNotNeedRead){
+		if(!v){
+			fmt.Println(translateString(os.Args[i], sourceLang))
 		}
 	}
 }
@@ -31,3 +50,4 @@ func arrayContains(arr []string, s string) int{
 	}
 	return -1
 }
+
